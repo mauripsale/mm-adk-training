91347e88115f268e87bf22fd5570b41ad7d3e6d4
Andrea Paterlini <paterlini.a@mmfg.it>
2025-10-22 13:31:18 +0200

    fix

--- 03_agent/agent.py
+++ 03_agent/agent.py
@@ -1,15 +1,6 @@
 from google.adk.agents.llm_agent import Agent
 from google.adk.tools import google_search
 
-
-root_agent = Agent(
-    model='gemini-2.5-flash',
-    name='root_agent',
-    description='A helpful assistant for user questions.',
-    instruction='Answer user lsquestions to the best of your knowledge. Search on Google if you are not sure about the answer.',
-    tools=[google_search],
-)
-
 def get_weather(city: str, unit: str):
     """
     Retrieves the weather for a city in the specified unit.
@@ -20,3 +11,11 @@ def get_weather(city: str, unit: str):
     """
     # ... function logic ...
     return {"status": "success", "report": f"Weather for {city} is sunny."}
+
+root_agent = Agent(
+    model='gemini-2.5-flash',
+    name='root_agent',
+    description='A helpful assistant for user questions.',
+    instruction='Answer user questions to the best of your knowledge. Use the tool get_weather when you can answer. parla in modo aulico',
+    tools=[get_weather],
+)
